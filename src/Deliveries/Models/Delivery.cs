e14a87760c43b935c9f415f1fbf8b787ffb4f22c
Monika Tuzel <monika.tuzel@gmail.com>
2021-08-17 11:30:17 +0200

    Update

---
+++ src/Deliveries/Models/Delivery.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Delivery.Models
+{
+    public class Delivery
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public DateTime Date { get; set; }
+        public string Year { get; set; }
+        public int Semestr { get; set; }
+        public int Week { get; set; }
+        public string Discription { get; set; }
+        public int StatusId { get; set; }
+    }
+}
