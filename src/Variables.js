d822fd394b52cf3aa5eb96dd31c86f520ceac2ff
Monika Tuzel <monika.tuzel@gmail.com>
2022-02-13 18:08:54 +0100

    Update

--- src/Variables.js
+++ src/Variables.js
@@ -1,33 +1,33 @@
 export const variables={
-    API_URL_USERS:"https://localhost:5021/users",
-    API_URL_USERS_LOGIN:"https://localhost:5021/users/account/login",
-    API_URL_USERS_REGISTER:"https://localhost:5021/users/account/register",
+    API_URL_USERS:" https://2b00-31-60-1-102.ngrok.io /users",
+    API_URL_USERS_LOGIN:" https://2b00-31-60-1-102.ngrok.io /users/account/login",
+    API_URL_USERS_REGISTER:" https://2b00-31-60-1-102.ngrok.io /users/account/register",
 
 
-    API_URL_MESSAGE:"https://localhost:5021/message",
-    API_URL_MESSAGE_SEND:"https://localhost:5021/message/send",
-    API_URL_MESSAGE_READ:"https://localhost:5021/message/read",
-    API_URL_MESSAGE_REPLY:"https://localhost:5021/message/reply",
+    API_URL_MESSAGE:" https://2b00-31-60-1-102.ngrok.io /message",
+    API_URL_MESSAGE_SEND:" https://2b00-31-60-1-102.ngrok.io /message/send",
+    API_URL_MESSAGE_READ:" https://2b00-31-60-1-102.ngrok.io /message/read",
+    API_URL_MESSAGE_REPLY:" https://2b00-31-60-1-102.ngrok.io /message/reply",
 
 
-    API_URL_TOWNS:"https://localhost:5021/towns",
-    API_URL_ADD_TOWN:"https://localhost:5021/contacts/AddNewTown",
+    API_URL_TOWNS:" https://2b00-31-60-1-102.ngrok.io /towns",
+    API_URL_ADD_TOWN:" https://2b00-31-60-1-102.ngrok.io /contacts/AddNewTown",
 
 
-    API_URL_ADRESS:"https://localhost:5021/contacts",
+    API_URL_ADRESS:" https://2b00-31-60-1-102.ngrok.io /contacts",
 
-    API_URL_ADRESS_BROWSE:"https://localhost:5021/addresses_browse",
+    API_URL_ADRESS_BROWSE:" https://2b00-31-60-1-102.ngrok.io /addresses_browse",
 
-    API_URL_DELIVERY:"https://localhost:5021/delivery",
-    API_URL_DELIVERY_BROWSE:"https://localhost:5021/delivery",
-    API_URL_DELIVERY_HISTORY:"https://localhost:5021/delivery/history",
-    API_URL_DELIVERY_CREATE:"https://localhost:5021/delivery/create",
+    API_URL_DELIVERY:" https://2b00-31-60-1-102.ngrok.io /delivery",
+    API_URL_DELIVERY_BROWSE:" https://2b00-31-60-1-102.ngrok.io /delivery",
+    API_URL_DELIVERY_HISTORY:" https://2b00-31-60-1-102.ngrok.io /delivery/history",
+    API_URL_DELIVERY_CREATE:" https://2b00-31-60-1-102.ngrok.io /delivery/create",
 
-    API_URL_DOCUMENTS_USER:"https://localhost:5021/files/browse",
-    API_URL_DOCUMENTS_BROWSE:"https://localhost:5021/files",
-    API_URL_DOCUMENTS_DOWNLOAD:"https://localhost:5021/files/download",
-    API_URL_DOCUMENTS_NEWUPLOAD:"https://localhost:5021/files/newUpload",
-    API_URL_DOCUMENTS_UPDATE:"https://localhost:5021/files/upload",
+    API_URL_DOCUMENTS_USER:" https://2b00-31-60-1-102.ngrok.io /files/browse",
+    API_URL_DOCUMENTS_BROWSE:" https://2b00-31-60-1-102.ngrok.io /files",
+    API_URL_DOCUMENTS_DOWNLOAD:" https://2b00-31-60-1-102.ngrok.io /files/download",
+    API_URL_DOCUMENTS_NEWUPLOAD:" https://2b00-31-60-1-102.ngrok.io /files/newUpload",
+    API_URL_DOCUMENTS_UPDATE:" https://2b00-31-60-1-102.ngrok.io /files/upload",
 
 
 
